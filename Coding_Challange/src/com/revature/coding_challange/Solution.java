ab8ab607a6cb7e1740035d24e74ccb414f04c118
Pradhan <dasp@sunypoly.edu>
2019-09-09 14:32:09 -0400

    2nd commit

---
+++ Coding_Challange/src/com/revature/coding_challange/Solution.java
@@ -0,0 +1,28 @@
+package com.revature.coding_challange;
+
+import java.util.Vector;
+import java.util.Queue;
+import java.lang.String;
+
+
+public class Solution { 
+	public int minMutation(String start, String end, String[] bank) {
+        int count = 0;
+	}
+	public static void main ( String [] args) { 
+	
+        
+		while (!geneQue.empty()) {
+        	string gene = geneQue.front().first;
+        int min = geneQue.front().second;
+        }
+        
+        if(gene == end) {  
+            return min;
+        
+	}
+		
+  }
+ 
+}
+
